642222226c1426f0db562053d756ff2fde2cfd43
hasan-huda <hasanhuda037@gmail.com>
2024-07-21 20:46:12 -0400

    remove kanga

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,9 +13,9 @@ export default function Home() {
                 <MaxWidthWrapper className="pb-24 pt-10 lg:grid lg:grid-cols-3 sm:pb-32 lg:gap-x-0 xl:gap-x-8 lg:pt-24 xl:pt-32 lg:pb-52">
                     <div className="col-span-2 px-6 lg:px-0 lg:pt-4">
                         <div className="relative mx-auto text-center lg:text-left flex flex-col items-center lg:items-start">
-                            <div className="absolute w-28 left-0 -top-20 hidden lg:block">
+                            {/* <div className="absolute w-28 left-0 -top-20 hidden lg:block">
                                 <img src="/kanga-1.png" className="w-full" />
-                            </div>
+                            </div> */}
                             <h1 className="relative w-fit tracking-tight text-balance mt-16 font-bold !leading-tight text-gray-900 text-5xl md:text-6xl lg:text-7xl">
                                 Discover Your{" "}
                                 <span className="bg-orange-600 px-2 text-white">
